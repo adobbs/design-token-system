013ca28dcca34c2522ae2171edd60a9183b27c90
Andy Dobbs <andrew.dobbs@gmail.com>
2025-06-20 09:21:14 -0400

    Fix run path

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ app = create_app()
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
-        "app.main:app", 
+        "main:app", 
         host="0.0.0.0", 
         port=settings.PORT, 
         reload=settings.DEBUG
